f2b20851394f2ccf87c692a8f267926fcfd48b36
abdallah abusnineh <124106124+Abdallahabusnineh@users.noreply.github.com>
2024-08-10 16:56:10 +0300

    .

--- lib/view/make_your_dream_interiors/make_dream_container_content.dart
+++ lib/view/make_your_dream_interiors/make_dream_container_content.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:land_interior_design/core/utils/custom_fontsize.dart';
 import 'package:land_interior_design/core/utils/is_responsive.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -14,40 +15,39 @@ class MakeDreamContainerContent extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 2.w),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: IsResponsive.isWebScreen(context)?CrossAxisAlignment.center:CrossAxisAlignment.start,
         children: [
           AutoSizeText(mainTitle,
             overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-            style: FontAppStyles.styleDarkPurpleW600( 15),),
-          SizedBox(height: IsResponsive.isWebScreen(context)?3.h:1.h,),
+            maxLines: 2,
+            style: FontAppStyles.styleDarkPurpleW600( getResponsiveFontSize(context, fontSize: 18)),),
+          SizedBox(height: IsResponsive.isMobileScreen(context)?1.h:5.h,),
           AutoSizeText(title1,
             overflow: TextOverflow.ellipsis,
             maxLines: 1,
-            style: FontAppStyles.stylePurpleWeight600( 11),),
+            style: FontAppStyles.stylePurpleWeight600( getResponsiveFontSize(context, fontSize: 15)),),
           AutoSizeText(subTitle1,
               overflow: TextOverflow.ellipsis,
               maxLines: 2,
-              style: FontAppStyles.styleBlackWeight400( 9)),
-          SizedBox(height: IsResponsive.isWebScreen(context)?3.h:1.h,),
+              style: FontAppStyles.styleBlackWeight400( getResponsiveFontSize(context, fontSize: 12))),
+          SizedBox(height: IsResponsive.isMobileScreen(context)?1.h:5.h,),
           AutoSizeText(title2,
             overflow: TextOverflow.ellipsis,
             maxLines: 1,
-            style: FontAppStyles.stylePurpleWeight600( 11),),
+            style: FontAppStyles.stylePurpleWeight600( getResponsiveFontSize(context, fontSize: 15)),),
           AutoSizeText(subTitle2,
               overflow: TextOverflow.ellipsis,
               maxLines: 2,
-              style: FontAppStyles.styleBlackWeight400( 9)),
-          SizedBox(height: IsResponsive.isWebScreen(context)?3.h:1.h,),
+              style: FontAppStyles.styleBlackWeight400( getResponsiveFontSize(context, fontSize: 12))),
+          SizedBox(height: IsResponsive.isMobileScreen(context)?1.h:5.h,),
           AutoSizeText(title3,
             overflow: TextOverflow.ellipsis,
             maxLines: 1,
-            style: FontAppStyles.stylePurpleWeight600( 11),),
+            style: FontAppStyles.stylePurpleWeight600( getResponsiveFontSize(context, fontSize: 15)),),
           AutoSizeText(subTitle3,
               overflow: TextOverflow.ellipsis,
               maxLines: 2,
-              style: FontAppStyles.styleBlackWeight400( 9)),
-          SizedBox(height: IsResponsive.isWebScreen(context)?3.h:1.h,),
+              style: FontAppStyles.styleBlackWeight400( getResponsiveFontSize(context, fontSize: 12))),
 
 
 
